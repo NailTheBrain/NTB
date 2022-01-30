e337cb36e20a48bd312d62f175e8bb723aeed89a
Abhishek Singhal <abhisinghal112001@gamil.com>
2022-01-30 10:07:55 +0530

    fix.3

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import App from "./App"
 import { BrowserRouter as Router } from "react-router-dom";
 import "./style.css"
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById("root"));
+ReactDOM.render(<Router basename="/sa"><App/></Router>, document.getElementById("root"));
+// basename = "/sa";
