74754b2bd3ebce76f2da9fd61b558f4ea111a955
Abhishek Singhal <abhisinghal112001@gamil.com>
2022-04-15 10:14:59 +0530

    Updates

---
+++ precache-manifest.80d3f19d57f8437909445e17b6ffafd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f37a0268a033006a501",
+    "url": "/NTB/static/css/main.59f0275e.chunk.css"
+  },
+  {
+    "revision": "9f37a0268a033006a501",
+    "url": "/NTB/static/js/main.9f37a026.chunk.js"
+  },
+  {
+    "revision": "56991b32096f6b409e8f",
+    "url": "/NTB/static/js/1.56991b32.chunk.js"
+  },
+  {
+    "revision": "796be353c8b033cfbd2f",
+    "url": "/NTB/static/js/runtime~main.796be353.js"
+  },
+  {
+    "revision": "48108c7890f31966ce43c054b611d9e8",
+    "url": "/NTB/static/media/back.48108c78.jpeg"
+  },
+  {
+    "revision": "7a104756b736057882fef7d9f3ae32be",
+    "url": "/NTB/static/media/logo.7a104756.png"
+  },
+  {
+    "revision": "129e13c68353a32ddde8a5d015de7fd8",
+    "url": "/NTB/static/media/img-2.129e13c6.png"
+  },
+  {
+    "revision": "c4b5cdcdd7fbb588d4b51e936163a87f",
+    "url": "/NTB/static/media/img-3.c4b5cdcd.png"
+  },
+  {
+    "revision": "113816666d93001e58ac14b286ed5c5b",
+    "url": "/NTB/static/media/img-4.11381666.png"
+  },
+  {
+    "revision": "5a00303364c4dba78fce0665ffa877c8",
+    "url": "/NTB/static/media/bk-3.5a003033.jpeg"
+  },
+  {
+    "revision": "96b998b42818707564c0f972040a5006",
+    "url": "/NTB/static/media/bk-5.96b998b4.jpg"
+  },
+  {
+    "revision": "1b66036e5cd8369e757c892c9d915ee4",
+    "url": "/NTB/static/media/bk-6.1b66036e.jpg"
+  },
+  {
+    "revision": "aa3b2ddd5682e0895b176a89953b7bf6",
+    "url": "/NTB/static/media/img-1.aa3b2ddd.png"
+  },
+  {
+    "revision": "1d1a8be9d24b5f29d376209c3635141e",
+    "url": "/NTB/index.html"
+  }
+];
